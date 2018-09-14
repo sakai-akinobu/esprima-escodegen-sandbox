2eb23bc9ab37ad0b5ada66ef515a6e18f6b8f0d8
sakai-akinobu <sakai.akinobu@gmail.com>
2018-09-14 17:26:27 +0900

    Add tests.

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ import {describe, it} from 'mocha';
 import assert from 'power-assert';
 import {parseScript} from 'esprima';
 import {generate} from 'escodegen';
+import {traverse} from 'estraverse';
+import * as types from 'ast-types';
 
 describe('index.js', function() {
   describe('parse <=> generate', function() {
@@ -47,11 +49,48 @@ describe('index.js', function() {
       });
     });
 
-    describe('escodegen', function() {
+    describe('generate', function() {
       it('generate', function() {
         const generatedSource = generate(ast);
         assert.strictEqual(generatedSource, source);
       });
     });
   });
+
+  describe('transform', function() {
+    it('transform variable name', function() {
+      const source = 'const foo = 1;';
+      const ast = parseScript(source);
+      traverse(ast, {
+        enter: node => {
+          if (node.type === 'VariableDeclarator') {
+            node.id = types.builders.identifier('bar');
+          }
+        },
+      });
+      const generatedSource = generate(ast);
+      assert.strictEqual(generatedSource, 'const bar = 1;');
+    });
+
+    it('transform function name', function() {
+      const source = 'function foo() { return 1; }';
+      const ast = parseScript(source);
+      traverse(ast, {
+        enter: node => {
+          if (node.type === 'FunctionDeclaration') {
+            node.id = types.builders.identifier('bar');
+          }
+        },
+      });
+      const generatedSource = generate(ast);
+      assert.strictEqual(generatedSource,
+        [
+          'function bar() {',
+          '    return 1;',
+          '}'
+        ].join('\n')
+      );
+    });
+  });
+
 });
